fce4b766aaa8182003007bc5c742893560a472ba
KC31415 <138476214+KC31415@users.noreply.github.com>
2023-07-11 19:56:45 +0800

    Add files via upload

--- day5.py
+++ day5.py
@@ -30,6 +30,22 @@ cols = file.readline() # gets first line of data
 data = file.readlines()
 file.close()
 
+fo = open("index1.html","w")
+fo.write("<html>")
+fo.write("<h1>Women and Children first?</h1>")
+fo.write("<p>This is the result of our Titanic analysis.</p>")
+fo.write("<img src='titanic.jpeg' width='500px'>")
+fo.write("<br>")
+fo.write("<h2>Study conclusion</h2>")
+fo.write("<svg width='800' height='500'>")
+fo.write("<rect width='740' height='100' fill='#ff0303'></rect>")
+fo.write("<text x='25' y='25' fill='white'>Women</text>")
+fo.write("<rect y='125' width='590' height='100' fill='green'></rect>")
+fo.write("<rect y='250' width='190' height='100' fill='yellow'></rect>")
+fo.write("</svg>")
+fo.write("</html>")
+fo.close()
+
 #create 'game loop' using while
 
 keepgoing = True
